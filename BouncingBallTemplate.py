69720c739e2049707adcf4961c1952a4f2e3881f
austin cove <nitsua8@gmail.com>
2015-06-10 14:25:08 -0400

    removed balls, added missiles and background

--- BouncingBallTemplate.py
+++ BouncingBallTemplate.py
@@ -9,6 +9,7 @@ class Sprite():
         self.ballrect = self.image.get_rect()
         self.ballrect.y = height - self.ballrect.height
         self.speed = speed
+        self.show = True
 
     def move(self, screen):
         self.ballrect = self.ballrect.move(self.speed)
@@ -16,7 +17,9 @@ class Sprite():
             self.speed[0] = -self.speed[0]
         if self.ballrect.top < 0 or self.ballrect.bottom > height:
             self.speed[1] = -self.speed[1]
-        screen.blit(self.image, self.ballrect)
+
+        if self.show:
+            screen.blit(self.image, self.ballrect)
 
 class Player(Sprite):
     def moveleft(self):
@@ -33,18 +36,39 @@ class Player(Sprite):
 
         screen.blit(self.image, self.ballrect)
 
+class Missile(Sprite):
+
+
+    def move(self, screen):
+        self.ballrect = self.ballrect.move(self.speed)
+        if self.ballrect.top < 1:
+            self.show = False
+
+        if self.show:
+            screen.blit(self.image, self.ballrect)
+
+
+        
+      
+
+
+background_image = pygame.image.load("MoutainsBackground.JPG")
 
 pygame.init()
-
 
 
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
-new_balls = [Sprite("ball5.JPG", [5, 5]), Sprite("ball.png", [2, 2]), Sprite("ball2.PNG", [3, 3]), Sprite("ball3.GIF", [1, 1]), Sprite("Ball4.PNG", [4, 4])] 
+
+#new_balls = [Sprite("ball.png", [2, 2]), Sprite("ball2.PNG", [3, 3]), Sprite("ball3.GIF", [1, 1])] 
 
 sapce_ship = Player("SpaceShip.PNG", [3, 0])
+
+missile = Missile("Missile.PNG", [0, -14])
+
+missile.show = False 
 
 while 1:
     for event in pygame.event.get():
@@ -55,10 +79,19 @@ while 1:
             if event.key == pygame.K_RIGHT:
                 sapce_ship.moveright()
 
-    screen.fill(black)
+            if event.key == pygame.K_SPACE:
+                missile.show = True 
+                missile.ballrect.x = sapce_ship.ballrect.x + sapce_ship.ballrect.width /2 - missile.ballrect.width / 2
+                missile.ballrect.y = height - missile.ballrect.height
+            
 
-    for b in new_balls:
-        b.move(screen)
+    screen.blit(background_image, [0, 0])
+    #screen.fill(black)
+
+
+   # for b in new_balls:
+    
+    missile.move(screen)
    
     sapce_ship.move(screen)
 
